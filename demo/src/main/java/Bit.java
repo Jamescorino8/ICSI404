fafa8db45e2dbc493cafc6ecca7614368957afe0
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2026-01-27 17:55:04 -0500

    Complete the Bit class

--- demo/src/main/java/Bit.java
+++ demo/src/main/java/Bit.java
@@ -1,42 +1,52 @@
 public class Bit {
-    public enum boolValues { FALSE, TRUE }
+    private boolean value;
 
     public Bit(boolean value) {
-
+        this.value = value;
     }
 
     public boolean getValue() {
-        return false;
+        return this.value;
     }
 
-    public void assign(boolValues value) {
+    public void assign(boolean value) {
+        this.value = value;
     }
 
     public void and(Bit b2, Bit result) {
+        and(this, b2, result);
+        
     }
 
     public static void and(Bit b1, Bit b2, Bit result) {
+        result.assign(b1.getValue() == false ? false : (b2.getValue() == false ? false : true));
     }
 
     public void or(Bit b2, Bit result) {
+        or(this, b2, result);
     }
 
     public static void or(Bit b1, Bit b2, Bit result) {
+        result.assign(b1.getValue() == false ? (b2.getValue() == false ? false : true) : true);
     }
 
     public void xor(Bit b2, Bit result) {
+        xor(this, b2, result);
     }
 
     public static void xor(Bit b1, Bit b2, Bit result) {
-    }
-
-    public static void not(Bit b2, Bit result) {
+        result.assign(b1.getValue() == false ? (b2.getValue() == false ? false : true) : (b2.getValue() == false ? true : false));
     }
 
     public void not(Bit result) {
+        not(this, result);
+    }
+
+    public static void not(Bit b1, Bit result) {
+        result.assign(b1.getValue() == false ? true : false);
     }
 
     public String toString() {
-        return "";
+        return value == true ? "1" : "0";
     }
 }
