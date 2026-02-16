0c7e4dbc9c6aeb34ff9b35f1a8dfc6191ae4cddb
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2026-02-16 15:33:59 -0500

    Complete Shifter class

--- demo/src/main/java/Shifter.java
+++ demo/src/main/java/Shifter.java
@@ -1,7 +1,29 @@
 public class Shifter {
     public static void LeftShift(Word32 source, int amount, Word32 result) {
+        /**
+         * Example (rshift by 2):
+         *  source: 0000 0000 0000 0001 0001 
+         *  result: 0000 0000 0000 0100 0100 (lshift by 2)
+         */
+        Bit currentBit = new Bit(false);
+        // i = source pointer, j = result pointer
+        for (int i = 31, j = 31 - amount; i >= 0 && j >= 0; i--, j--) {
+            source.getBitN(i, currentBit);
+            result.setBitN(j, currentBit);
+        }
     }
 
     public static void RightShift(Word32 source, int amount, Word32 result) {
+        /**
+         * Example (rshift by 2):
+         *  source: 0000 0000 0000 0001 0001 
+         *  result: 0000 0000 0000 0000 0100 
+         */
+        Bit currentBit = new Bit(false);
+        // i = source pointer, j = result pointer
+        for (int i = 31 - amount, j = 31; i >= 0 && j >= 0; i--, j--) {
+            source.getBitN(i, currentBit);
+            result.setBitN(j, currentBit);
+        }
     }
 }
