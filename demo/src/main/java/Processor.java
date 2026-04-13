281a3bf697065bf9cd2891a5eb6f74371de271d0
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2026-04-13 10:27:06 -0400

    implement execute and store methods and complete Processor class

--- demo/src/main/java/Processor.java
+++ demo/src/main/java/Processor.java
@@ -5,7 +5,8 @@ import java.util.Stack;
 public class Processor {
     private Memory mem;
     public List<String> output = new LinkedList<>();
-    int programCounter = 0;
+    int programCounter = 0; // PC
+    boolean pcUpdated = false; // True if execute updated PC
     Word16 instructionRegister;
     int opcode, mode, opA, opB;
     Word32[] registers = new Word32[32];
@@ -13,8 +14,7 @@ public class Processor {
     Word32 op2 = new Word32(); // decoded operand 2
     Word32 result = new Word32(); // result from execute
     ALU alu = new ALU();
-    Stack<Integer> callStack = new Stack<>();
-    boolean pcUpdated = false; // True if execute updated the PC
+    Stack<Integer> callStack = new Stack<>(); // Stack of return addresses for call/return instructions
 
     public Processor(Memory m) {
         mem = m;
@@ -36,12 +36,32 @@ public class Processor {
 
     // read instruction from memory
     private void fetch() {
+        // Clear value so null dram entries read as zero (halt)
+        new Word32().copy(mem.value);
+
         Word32 addr = new Word32();
         TestConverter.fromInt(programCounter, addr);
         mem.address = addr;
-
         mem.read();
-        mem.value.getTopHalf(instructionRegister);
+
+        // If top 16 bits are zero and PC > 0, the instruction may be in the
+        // bottom half of the previous merged word (sliding-window encoding)
+        Bit b = new Bit(false);
+        boolean topHalfIsZero = true;
+        for (int i = 0; i < 16; i++) {
+            mem.value.getBitN(i, b);
+            if (b.getValue()) { topHalfIsZero = false; break; }
+        }
+
+        if (topHalfIsZero && programCounter > 0) {
+            new Word32().copy(mem.value);
+            TestConverter.fromInt(programCounter - 1, addr);
+            mem.address = addr;
+            mem.read();
+            mem.value.getBottomHalf(instructionRegister);
+        } else {
+            mem.value.getTopHalf(instructionRegister);
+        }
     }
 
     private void decode() {
@@ -50,7 +70,7 @@ public class Processor {
         StringBuilder opABits = new StringBuilder(5);
         StringBuilder opBBits = new StringBuilder(5);
         Bit temp = new Bit(false);
-        
+
         for (int i = 0; i < 16; i++) {
             instructionRegister.getBitN(i, temp);
             if (i < 5) {
@@ -72,32 +92,32 @@ public class Processor {
         // fill op1 and op2
         switch(opcode) {
             case 0: case 8: case 10:
-                break; 
-            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 11:
+                break;
+            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 11: case 18: case 20:
                 // 2 arg
+                registers[opB].copy(op1);
+                if (mode == 0) {
+                    registers[opA].copy(op2);
+                } else {
+                    int imm5 = (opA & 0x10) != 0 ? opA - 32 : opA; // 5 bit signed immediate value
+                    TestConverter.fromInt(imm5, op2);
+                }
+                break;
+            case 19:
+                // store
                 if (mode == 0) {
                     registers[opA].copy(op1);
                 } else {
-                    // sign extend 5bit immediate. negative if bit 4 set
-                    int imm5 = (opA & 0x10) != 0 ? opA-32 : opA;
+                    int imm5 = (opA & 0x10) != 0 ? opA - 32 : opA; // 5 bit signed immediate value
                     TestConverter.fromInt(imm5, op1);
                 }
                 registers[opB].copy(op2);
                 break;
             case 9: case 12: case 13: case 14: case 15: case 16: case 17:
-                // 1 arg
+                // 1 arg 
                 int raw = (opA << 5) | opB;
-                int imm10 = (raw & 0x200) != 0 ? raw - 1024 : raw;
+                int imm10 = (raw & 0x200) != 0 ? raw - 1024 : raw; // 10 bit signed immediate value
                 TestConverter.fromInt(imm10, op1);
-                break;
-            case 18:
-                // load
-                registers[opA].copy(op1);
-                break;
-            case 19:
-                // store
-                registers[opA].copy(op1);
-                registers[opB].copy(op2);
                 break;
         }
     }
@@ -107,11 +127,11 @@ public class Processor {
 
         switch(opcode) {
             case 0:
-                // halt
+                // Halt
                 output.add("halt");
                 break;
             case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 11:
-                // ALU ops
+                // ALU ops (add, subtract, multiply, shifts, bitwise, compare)
                 instructionRegister.copy(alu.instruction);
                 op1.copy(alu.op1);
                 op2.copy(alu.op2);
@@ -119,86 +139,90 @@ public class Processor {
                 alu.result.copy(result);
                 break;
             case 8:
-                // syscall
-                String val = String.valueOf(TestConverter.toInt(registers[0]));
-                output.add(val);
-                System.out.println(val);
+                // Syscall
+                if (opB == 0) {
+                    printReg();
+                } else if (opB == 1) {
+                    printMem();
+                }
                 break;
             case 9:
-                // call
+                // Call: Pushes the current address + 1 on the stack. Sets PC to PC + Immediate
                 callStack.push(programCounter + 1);
-                programCounter = TestConverter.toInt(op1);
+                programCounter = programCounter + 2 * TestConverter.toInt(op1) - 1;
                 pcUpdated = true;
                 break;
             case 10:
-                // return
+                // Return: Pops from the stack and sets the PC to the popped value
                 programCounter = callStack.pop();
                 pcUpdated = true;
                 break;
             case 12:
-                // ble
+                // BLE
                 if (alu.less.getValue() || alu.equal.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 13:
-                // blt
+                // BLT
                 if (alu.less.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 14:
-                // bge
+                // BGE
                 if (!alu.less.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 15:
-                // bgt
+                // BGT
                 if (!alu.less.getValue() && !alu.equal.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 16:
-                // beq
+                // BEQ
                 if (alu.equal.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 17:
-                // bne
+                // BNE
                 if (!alu.equal.getValue()) {
-                    programCounter += TestConverter.toInt(op1);
+                    programCounter += 2 * TestConverter.toInt(op1) - 1;
                     pcUpdated = true;
                 }
                 break;
             case 18:
-                // load
-                op1.copy(mem.address);
+                // Load
+                Word32 loadAddr = new Word32();
+                Adder.add(op1, op2, loadAddr);
+                loadAddr.copy(mem.address);
                 mem.read();
                 mem.value.copy(result);
                 break;
             case 19:
-                // store
+                // Store
                 op2.copy(mem.address);
                 op1.copy(mem.value);
                 mem.write();
                 break;
             case 20:
-                // copy
-                op1.copy(result);
+                // Copy
+                op2.copy(result);
                 break;
         }
     }
 
     private void printReg() {
         for (int i = 0; i < 32; i++) {
-            var line = "r"+ i + ":" + ""; // TODO: add the register value here...
+            var line = "r" + i + ":" + registers[i].toString();
             output.add(line);
             System.out.println(line);
         }
@@ -207,18 +231,27 @@ public class Processor {
     private void printMem() {
         for (int i = 0; i < 1000; i++) {
             Word32 addr = new Word32();
-            Word32 value = new Word32();
-            // Convert i to Word32 here...
+            TestConverter.fromInt(i, addr);
             addr.copy(mem.address);
             mem.read();
+            Word32 value = new Word32();
             mem.value.copy(value);
-            var line = i + ":" + value + "(" + TestConverter.toInt(value) + ")";
+            var line = i + ":" + value.toString() + "(" + TestConverter.toInt(value) + ")";
             output.add(line);
             System.out.println(line);
         }
     }
 
     private void store() {
-        
+        // Write result to registers[opB]
+        switch (opcode) {
+            // opcodes that produce a new value
+            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 18: case 20: 
+                result.copy(registers[opB]);
+                break;
+        }
+        if (!pcUpdated) {
+            programCounter++;
+        }
     }
 }
